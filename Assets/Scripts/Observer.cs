d7cfa5f71d660a2ac3857e51027b8cb4a930fb5a
Sharlotte <seeseafood@naver.com>
2023-02-25 22:19:07 +0900

    Initial commit

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player;
+    public GameEnding gameEnding;
+
+    bool m_IsPlayerInRange;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+            m_IsPlayerInRange = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+            m_IsPlayerInRange = false;
+    }
+
+    void Update()
+    {
+        if (m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new(transform.position, direction);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit) && raycastHit.collider.transform == player)
+            {
+                gameEnding.CaughtPlayer();
+            }
+        }
+    }
+}
