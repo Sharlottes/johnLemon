6ab264f3a4a9b93842159969f36d1c616c9f2526
Sharlotte <aaa9810321@gmail.com>
2023-02-26 07:57:56 +0900

    적 AI 개발 (#12)
    * Feat/issue 3 (#11)
    
    * feat: #4 TMP 임포트
    
    * style: #4 Canvas로 재명명
    
    * feat: #4 플레이어 이름 UI 추가
    
    * fix: #4 한글 폰트 깨짐 해결
    
    * fix: #4 플레이어 이름 위치 재조정
    
    * feat: #4 탈출UI에 버튼 추가, 레이어 이슈 해결
    
    * doc: #4 TODO 갱신
    
    * feat: #6 싱글톤MB 클래스 구조 추가
    
    * feat: #6 플레이어 싱글톤 추가
    
    * refactor: #6 디렉토리 정리 / 네임스페이스 추가
    
    * refactor: #6 GameEnding 싱글톤화 -> 코드 정리
    
    * feat: #6 유령 타임아웃 어그로 추가
    
    * doc: #6 TODO 갱신

---
+++ Assets/Scripts/Structs/Singletons/DDOLSingletonMonoBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Structs.Singleton
+{
+    public class DDOLSingletonMonoBehaviour<T> : SingletonMonoBehaviour<T> where T : MonoBehaviour
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
