f8af37debd366f3f5f3f7016d01e50f81005937a
Sharlotte <seeseafood@naver.com>
2023-02-26 00:16:45 +0900

    fix: #4 플레이어 이름 위치 재조정

--- Assets/PlayerName.cs
+++ Assets/PlayerName.cs
@@ -8,12 +8,19 @@ public class PlayerName : MonoBehaviour
     public Canvas canvas;
     public string playerName;
     public GameObject nameTextPref;
+    public float nameTextFollowSpeed = 5;
+
     TMP_Text m_nameText;
 
     void Start()
     {
         m_nameText = Instantiate(nameTextPref, canvas.transform).GetComponent<TMP_Text>();
         m_nameText.SetText(playerName);
-        m_nameText.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(1, 1.35f));
+        m_nameText.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 1.5f));
+    }
+
+    private void Update()
+    {
+        m_nameText.transform.position = Vector3.Lerp(m_nameText.transform.position, Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 1.5f)), nameTextFollowSpeed * Time.deltaTime);
     }
 }
