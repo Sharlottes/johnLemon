9dbee12c16d0b45ab191b947b5848427da3229e1
Sharlotte <seeseafood@naver.com>
2023-03-02 12:10:50 +0900

    feat: 에디터 모드에서도 겜오버 시 나가지도록

--- Assets/Scripts/UI/Scenes/GameOverScene/ExitButton.cs
+++ Assets/Scripts/UI/Scenes/GameOverScene/ExitButton.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Utils.Transitions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,16 @@ namespace Assets.Scripts.UI.Scenes.GameOverScene
 {
     public class ExitButton : MonoBehaviour
     {
-        public void OnClick() => Application.Quit();
+        public void OnClick() => StartCoroutine(GameExit());
+
+        IEnumerator GameExit()
+        {
+            yield return ScreenTransitionController.Instance.StartTransition<ScreenFadeInTransition>(1);
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
     }
 }
